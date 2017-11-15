5d91814cff1ac42655bb63c6d890b4305c1b094c
diegodelaefe <ksiris56@gmail.com>
2017-11-14 21:27:06 -0300

    v0.2 deteccion de brillo y inversion eje x

--- sketch.js
+++ sketch.js
@@ -1,16 +1,19 @@
 /*
-//  Detector de movimiento 
+//  Detector de movimiento
 //
 */
 
 let video;
 let vidPix = [];
+let prevPix;
 let escala = 32;
 
 function setup() {
   createCanvas(640, 480, P2D);
   background(20);
   pixelDensity(1);
+  ellipseMode(CENTER);
+  noStroke();
 
   video = createCapture(VIDEO);
   video.size(width / escala, height / escala);
@@ -18,7 +21,7 @@ function setup() {
   for (let i = 0; i < width / escala; i++) {
     vidPix[i] = [];
   }
-
+  prev = createImage(width / escala, height / escala, RGB);
   video.hide();
 }
 
@@ -30,29 +33,30 @@ function draw() {
   for (let y = 0; y < video.height; y++) {
     for (let x = 0; x < video.width; x++) {
 
-      fill(vidPix[x][y]);
-      rect(x * escala, y * escala, escala, escala);
+      if (vidPix[x][y] > 200) {
+        fill(vidPix[x][y]);
+        ellipse(x * escala, y * escala, escala, escala);
+      }
     }
   }
 
 }
 
-function pixelar(){
+function pixelar() {
   video.loadPixels();
   loadPixels()
 
   for (let y = 0; y < video.height; y++) {
     for (let x = 0; x < video.width; x++) {
-      this.index = (x + y * video.width) * 4;
+      this.index = ((video.width-x-1) + y * video.width) * 4;
       this.r = video.pixels[index + 0];
       this.g = video.pixels[index + 1];
       this.b = video.pixels[index + 2];
 
+
       this.brillo = (r + g + b) / 3; //brillo a partir de suma de colores
       vidPix[x][y] = brillo;
 
-      fill(brillo);
-      rect(x * escala, y * escala, escala, escala);
     }
   }
 }
